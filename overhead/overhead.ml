9936ab71a1c297a38de6485bc800a53303e515e5
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-04-13 20:22:58 +0100

    added -group and -commit-once options

--- overhead/overhead.ml
+++ overhead/overhead.ml
@@ -57,21 +57,24 @@ let get_content content key =
  * -image[-u] path (image's path)
  * -cnt messages (number of messages)
  *)
-let rec args i content start cnt =
+let rec args i content start cnt group once =
   if i >= Array.length Sys.argv then
-    (content,start,cnt)
+    (content,start,cnt,group, once)
   else
     match Sys.argv.(i) with
-    | "-ascii" -> args (i+2) (Some (`Ascii (int_of_string Sys.argv.(i+1)))) start cnt
-    | "-ascii-u" -> args (i+2) (Some (`AsciiU (int_of_string Sys.argv.(i+1)))) start cnt
-    | "-image" -> args (i+2) (Some (`Image Sys.argv.(i+1))) start cnt
-    | "-image-u" -> args (i+2) (Some (`ImageU Sys.argv.(i+1))) start cnt
-    | "-start" -> args (i+2) content (Some (int_of_string Sys.argv.(i+1))) cnt
-    | "-cnt" -> args (i+2) content start (Some (int_of_string Sys.argv.(i+1)))
+    | "-ascii" -> args (i+2) (Some (`Ascii (int_of_string Sys.argv.(i+1)))) start cnt group once
+    | "-ascii-u" -> args (i+2) (Some (`AsciiU (int_of_string Sys.argv.(i+1)))) start cnt group once
+    | "-image" -> args (i+2) (Some (`Image Sys.argv.(i+1))) start cnt group once
+    | "-image-u" -> args (i+2) (Some (`ImageU Sys.argv.(i+1))) start cnt group once
+    | "-start" -> args (i+2) content (Some (int_of_string Sys.argv.(i+1))) cnt group once
+    | "-cnt" -> args (i+2) content start (Some (int_of_string Sys.argv.(i+1))) group once
+    | "-group" -> args (i+2) content start cnt (Some (int_of_string Sys.argv.(i+1))) once
+    | "-commit-once" -> args (i+1) content start cnt group true
     | _ -> raise InvalidCommand
 
 let usage () =
-  Printf.printf "usage: overhead [-ascii[-u] size|-image[-u] path] -cnt num -start num\n%!"
+  Printf.printf "usage: overhead [-ascii[-u] size|-image[-u] path] -cnt num
+  -start num -group num -commit-once\n%!"
 
 module Option = struct
   let value_exn = function None -> raise Not_found | Some x -> x
@@ -80,37 +83,64 @@ end
 
 let commands f =
   try
-    let (content,start,cnt) = args 1 None None None in
+    let (content,start,cnt,group,once) = args 1 None None None None false in
     if content = None || cnt = None then (
       usage ();
       return ()
     ) else
       f (Option.value_exn content) (Option.value start ~default:0)
-        (Option.value_exn cnt)
+        (Option.value_exn cnt) group once
   with InvalidCommand -> usage (); return ()
 
 let pr store key =
-  Store.read_exn (store "") ["root"; string_of_int key] >>= fun v ->
+  Store.read_exn (store "reading store") ["root"; string_of_int key] >>= fun v ->
   Printf.printf "value at key %d %s\n%!" key v;
   return ()
 
+let get_key start i = function
+  | None -> 
+    let key = string_of_int (start + i) in
+    ([key], key)
+  | Some group -> 
+    let key1 = string_of_int ((start + i) / group) in
+    let key2 = string_of_int ((mod) (start + i) group) in
+    ([key1;key2], key1 ^ ":" ^ key2)
+
 let () =
   Lwt_main.run (
-    commands (fun content start cnt ->
+    commands (fun content start cnt group once ->
     let config = Irmin_git.config ~root:"/tmp/irmin/test" ~bare:true () in
     Store.create config task >>= fun store ->
+    begin
+      if once then
+        View.of_path (store "view of path") ["root"] >>= fun view -> return (Some view)
+      else
+        return None
+    end >>= fun gview ->
     let rec fill i =
       if i > cnt then
         return ()
       else (
-        View.of_path task (store "") ["root"] >>= fun view ->
+        begin 
+          match gview with
+          | Some view -> return view
+          | None -> View.of_path (store "view of path") ["root"] 
+        end >>= fun view ->
         get_content content (start+i) >>= fun bytes ->
-        Printf.printf "%07d %d\n%!" (start+i) (Bytes.length bytes);
-        View.update (view "") [string_of_int i] bytes >>= fun () ->
-        View.update_path "" store ["root"] view >>= fun () ->
-        fill (i + 1)
+        let (key,str) = get_key start i group in
+        Printf.printf "%7s %d\n%!" str (Bytes.length bytes);
+        View.update view key bytes >>= fun () ->
+        begin
+          match gview with
+          | Some _ -> return ()
+          | None -> View.update_path (store "updating store") ["root"] view 
+        end >>= fun () ->
+        fill (i + 1) 
       )
     in
-    fill 1
+    fill 1 >>= fun () ->
+    match gview with
+    | Some view -> View.update_path (store "updating store") ["root"] view
+    | None -> return ()
     )
   )
