64beec7402efc772363f4e0a7dfeb0ad2a667367
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-04-14 22:18:10 +0100

    added option to repeat tests

--- overhead/overhead.ml
+++ overhead/overhead.ml
@@ -57,25 +57,26 @@ let get_content content key =
  * -image[-u] path (image's path)
  * -cnt messages (number of messages)
  *)
-let rec args i content start cnt group once repo =
+let rec args i content start cnt group once repo repeat =
   if i >= Array.length Sys.argv then
-    (content,start,cnt,group,once,repo)
+    (content,start,cnt,group,once,repo,repeat)
   else
     match Sys.argv.(i) with
-    | "-ascii" -> args (i+2) (Some (`Ascii (int_of_string Sys.argv.(i+1)))) start cnt group once repo
-    | "-ascii-u" -> args (i+2) (Some (`AsciiU (int_of_string Sys.argv.(i+1)))) start cnt group once repo
-    | "-image" -> args (i+2) (Some (`Image Sys.argv.(i+1))) start cnt group once repo
-    | "-image-u" -> args (i+2) (Some (`ImageU Sys.argv.(i+1))) start cnt group once repo
-    | "-start" -> args (i+2) content (Some (int_of_string Sys.argv.(i+1))) cnt group once repo
-    | "-cnt" -> args (i+2) content start (Some (int_of_string Sys.argv.(i+1))) group once repo
-    | "-group" -> args (i+2) content start cnt (Some (int_of_string Sys.argv.(i+1))) once repo
-    | "-commit-once" -> args (i+1) content start cnt group true repo
-    | "-repo" -> args (i+2) content start cnt group once (Some Sys.argv.(i+1))
+    | "-ascii" -> args (i+2) (Some (`Ascii (int_of_string Sys.argv.(i+1)))) start cnt group once repo repeat
+    | "-ascii-u" -> args (i+2) (Some (`AsciiU (int_of_string Sys.argv.(i+1)))) start cnt group once repo repeat
+    | "-image" -> args (i+2) (Some (`Image Sys.argv.(i+1))) start cnt group once repo repeat
+    | "-image-u" -> args (i+2) (Some (`ImageU Sys.argv.(i+1))) start cnt group once repo repeat
+    | "-start" -> args (i+2) content (Some (int_of_string Sys.argv.(i+1))) cnt group once repo repeat
+    | "-cnt" -> args (i+2) content start (Some (int_of_string Sys.argv.(i+1))) group once repo repeat
+    | "-group" -> args (i+2) content start cnt (Some (int_of_string Sys.argv.(i+1))) once repo repeat
+    | "-commit-once" -> args (i+1) content start cnt group true repo repeat
+    | "-repo" -> args (i+2) content start cnt group once (Some Sys.argv.(i+1)) repeat
+    | "-repeat" -> args (i+2) content start cnt group once repo (int_of_string Sys.argv.(i+1))
     | _ -> raise InvalidCommand
 
 let usage () =
   Printf.printf "usage: overhead [-ascii[-u] size|-image[-u] path] -cnt num
-  -repo path [-start num] [-group num] [-commit-once]\n%!"
+  -repo path [-start num] [-group num] [-commit-once] [-repeat number]\n%!"
 
 module Option = struct
   let value_exn = function None -> raise Not_found | Some x -> x
@@ -84,13 +85,13 @@ end
 
 let commands f =
   try
-    let (content,start,cnt,group,once,repo) = args 1 None None None None false None in
+    let (content,start,cnt,group,once,repo,repeat) = args 1 None None None None false None 1 in
     if content = None || cnt = None || repo = None then (
       usage ();
       return ()
     ) else
       f (Option.value_exn content) (Option.value start ~default:0)
-        (Option.value_exn cnt) group once (Option.value_exn repo)
+        (Option.value_exn cnt) group once (Option.value_exn repo) repeat
   with InvalidCommand -> usage (); return ()
 
 let pr store key =
@@ -107,50 +108,84 @@ let get_key start i = function
     let key2 = string_of_int ((mod) (start + i) group) in
     ([key1;key2], key1 ^ ":" ^ key2)
 
+let du path =
+  let ci = Unix.open_process_in ("du -csh " ^ path) in
+  let str = Pervasives.input_line ci in
+  let _ = Unix.close_process_in ci in
+  let re = Re_posix.compile_pat "(^[\t ]*[0-9.]+)([KMG])" in
+  let s = Re.exec re str in
+  let size = float_of_string (Re.get s 1) in
+  int_of_float (
+    match (Re.get s 2) with
+    | "K" -> size
+    | "M" -> size *. 1000.
+    | "G" -> size *. 1000000.
+    | _ -> assert (false)
+  )
+
+let fs path =
+  let ci = Unix.open_process_in ("cd " ^ path ^ ";git reset master --hard") in
+  let rec readon () =
+    try
+      let _ = Pervasives.input_line ci in
+      readon ()
+    with End_of_file -> ()
+  in
+  readon ();
+  let root = Filename.concat path "root" in
+  let _ = Unix.close_process_in ci in
+  let size = du root in
+  let _ = Unix.system ("cd " ^ path ^ ";rm -rf " ^ root) in
+  size
+
 let () =
   Lwt_main.run (
-    commands (fun content start cnt group once repo ->
+    commands (fun content start cnt group once repo repeat ->
     let config = Irmin_git.config ~root:repo ~bare:true () in
     Store.create config task >>= fun store ->
-    begin
-      if once then
-        View.of_path (store "view of path") ["root"] >>= fun view -> return (Some view)
+    let rec repeat_times start n =
+      begin
+        if once then
+          View.of_path (store "view of path") ["root"] >>= fun view -> return (Some view)
+        else
+          return None
+      end >>= fun gview ->
+      let rec fill i origsize =
+        if i > cnt then
+          return origsize
+        else (
+          begin 
+            match gview with
+            | Some view -> return view
+            | None -> View.of_path (store "view of path") ["root"] 
+          end >>= fun view ->
+          get_content content (start+i) >>= fun bytes ->
+          let (key,str) = get_key start i group in
+          let len = Bytes.length bytes in
+          let origsize = origsize + len in
+          View.update view key bytes >>= fun () ->
+          begin
+            match gview with
+            | Some _ -> return ()
+            | None -> View.update_path (store "updating store") ["root"] view 
+          end >>= fun () ->
+          fill (i + 1) origsize
+        )
+      in
+      fill 1 0 >>= fun origsize ->
+      begin
+      match gview with
+      | Some view -> View.update_path (store "updating store") ["root"] view
+      | None -> return ()
+      end >>= fun () ->
+      let size = du (Filename.concat repo  ".git") in
+      let size_fs = fs repo in
+      Printf.printf "%d,%d,%d,%d\n" (cnt*n) ((origsize*n)/1000) size size_fs;
+      if n = repeat then
+        return ()
       else
-        return None
-    end >>= fun gview ->
-    let rec fill i origsize =
-      if i > cnt then
-        return origsize
-      else (
-        begin 
-          match gview with
-          | Some view -> return view
-          | None -> View.of_path (store "view of path") ["root"] 
-        end >>= fun view ->
-        get_content content (start+i) >>= fun bytes ->
-        let (key,str) = get_key start i group in
-        let len = Bytes.length bytes in
-        let origsize = origsize + len in
-        View.update view key bytes >>= fun () ->
-        begin
-          match gview with
-          | Some _ -> return ()
-          | None -> View.update_path (store "updating store") ["root"] view 
-        end >>= fun () ->
-        fill (i + 1) origsize
-      )
+        repeat_times (n * cnt) (n + 1) 
     in
-    fill 1 0 >>= fun origsize ->
-    Printf.printf "original size: %dKB\n%!" (origsize/1000);
-    begin
-    match gview with
-    | Some view -> View.update_path (store "updating store") ["root"] view
-    | None -> return ()
-    end >>= fun () ->
-    let ci = Unix.open_process_in ("du -csh " ^ (Filename.concat repo  ".git")) in
-    let str = Pervasives.input_line ci in
-    let _ = Unix.close_process_in ci in
-    Printf.printf "repo size: %s\n%!" str;
-    return ()
+    repeat_times start 1
     )
   )
